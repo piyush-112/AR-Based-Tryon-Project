c6ad1dbe1aec26f2d8026897ac916beb96f6e7f7
Piyush Chaudhari <piyushchaudhari112@gmail.com>
2024-08-27 23:23:06 +0530

    deleted files

--- ElearningFrontend/src/data/Regex.jsx
+++
@@ -1,8 +0,0 @@
-export const validEmail = new RegExp(
-  "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
-);
-export const validPassword = new RegExp("^[0-9]{1,6}$");
-
-export const onlyChar = new RegExp("^[A-Za-z]+$");
-
-export const onlyPhone = new RegExp("^[0-9]{10}$");
